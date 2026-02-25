3cb169e9393bb6fabd56032d16d404f51fdfdc29
SaraBarrett <sara.c.mon@gmail.com>
2026-02-25 09:45:43 +0000

    js start

---
+++ js/exs.js
@@ -0,0 +1,15 @@
+//p13
+//declarar variáveis
+// let myLuckyNumber = 16;
+// let myName = 'Sara';
+
+// alert('o meu nome é '+myName+ ' e o meu número preferido é o '+myLuckyNumber);
+
+//pp 24
+const MESSAGE = ' TASTE THE RAINBOW  ';
+
+let whisper = MESSAGE.toLowerCase().trim();
+alert(whisper)
+
+//pp25: ex extra
+let fullName = 'Sara Monteiro';
---
+++ js/script.js
@@ -0,0 +1,32 @@
+//primeira linha de código JS
+let firstCode = "hello world";
+let className = "Low Code";
+let number = 9;
+
+//reassignar um novo valor à variável
+className = "Design UI/UX";
+
+//concatenar variáveis
+//alert(className+': '+firstCode+'!');
+
+number = 8;
+//if/else: validar se número é par ou impar e informar
+//se for par
+// if (number % 2 == 0) {
+//   alert("número par");
+// } else {
+//   //se for impar
+//   alert("número impar");
+// }
+
+//comparação == e ===
+// let number1 = 16;
+// let number2= '16';
+
+// alert(number1 === number2)
+
+let firstName = "Sara";
+let lastName = "Monteiro";
+
+// alert('olá ' +firstName+' '+lastName[0]+'.');
+alert(firstName.toUpperCase().trim())
