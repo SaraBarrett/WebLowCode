e08f2927c8894c1087eaf0ed53a6350bf8e3de7f
SaraBarrett <sara.c.mon@gmail.com>
2026-02-26 11:13:08 +0000

    arrays

--- js/exs.js
+++ js/exs.js
@@ -6,10 +6,74 @@
 // alert('o meu nome é '+myName+ ' e o meu número preferido é o '+myLuckyNumber);
 
 //pp 24
-const MESSAGE = ' TASTE THE RAINBOW  ';
+// const MESSAGE = ' TASTE THE RAINBOW  ';
 
-let whisper = MESSAGE.toLowerCase().trim();
-alert(whisper)
+// let whisper = MESSAGE.toLowerCase().trim();
+// alert(whisper)
 
 //pp25: ex extra
-let fullName = 'Sara Monteiro';
+// let fullName = 'Sara Monteiro';
+
+// //procurar pelo index onde iremos fazer o 'corte'
+// let cutIndex = fullName.indexOf(' ');
+
+// //usando o método slice, partir o primeiro nome
+// let firstName = fullName.slice(0,cutIndex);
+
+// //usando o método slice, partir o último nome
+// let lastName = fullName.slice(cutIndex+1);
+// alert(lastName);
+
+//pp26
+// const WORD = 'skateboard';
+// let facialHair = WORD.slice(5).replace('o','e');
+// alert(facialHair);
+
+//pp 31
+// let myFirstName = prompt('qual é o teu primeiro nome?');
+// let mySurname = prompt('qual é o teu sobrenome?');
+
+// alert('olá '+ myFirstName + ' '+ mySurname);
+
+// 2. Crie um sistema que:
+
+// Peça o nome do utilizador
+// Peça a idade
+
+// Se for maior de idade, mostre um menu com indicações personalizadas (tratando pelo nome):
+// Ver saldo: simule com um alerta 'tem 1.0000 de euros'
+// Fazer levantamentos: pergunte ao utilizador o valor a levantar. No final do levantamento, indique quanto ficou em conta.
+// Depositar dinheiro: lance um alerta a indicar 'depósito de 600euros efectuado!'
+// Se for menor, mostre: "Acesso negado"
+
+let userName = prompt("qual é o seu nome?");
+let userAge = prompt("qual é a sua idade?");
+
+if (userAge > 17) {
+  //bloco de código para a condição maiores de idade
+  alert(userName + " tens acesso");
+
+  //perguntar ao user maior de idade que operação ele quer
+  let option = prompt(
+    "que operação pretende efectuar? \n 1-ver saldo \n 2-Fazer Levantamentos \n 3-depositar dinheiro",
+  );
+
+  switch (option) {
+    case "1":
+      alert(userName + " tens 10.000 de euros");
+      break;
+    case "2":
+      let value = prompt("quanto quer levantar?");
+      alert(
+        "levantou " + value + " ,tem disponível " + parseInt(10000 - value),
+      );
+      break;
+    case "3":
+      alert("depósito de 600euros efectuado!");
+      break;
+    default:
+      alert("Opção inválida");
+  }
+} else {
+  alert(userName + " lamento mas não tens acesso a esta funcionalidade");
+}
--- js/script.js
+++ js/script.js
@@ -7,7 +7,10 @@ let number = 9;
 className = "Design UI/UX";
 
 //concatenar variáveis
-//alert(className+': '+firstCode+'!');
+// alert(className + ": " + firstCode + "!");
+
+// //forma alternativa de concatenar variáveis
+// alert(`a turma é ${className}: ${firstCode}!`);
 
 number = 8;
 //if/else: validar se número é par ou impar e informar
@@ -25,8 +28,106 @@ number = 8;
 
 // alert(number1 === number2)
 
-let firstName = "Sara";
-let lastName = "Monteiro";
+// let firstName = "Sara";
+// let lastName = "Monteiro";
 
-// alert('olá ' +firstName+' '+lastName[0]+'.');
-alert(firstName.toUpperCase().trim())
+// // alert('olá ' +firstName+' '+lastName[0]+'.');
+// alert(firstName.toUpperCase().trim())
+
+//tipo de dados undefinied
+//alert(hello)
+
+//tipo de dados null
+// let hello = null;
+// alert(hello)
+
+//alert
+// alert('olá sou um alerta!')
+
+//console.log()
+// console.log('estou loggado na consola e sou amigo dos programadores')
+
+//perguntar algo ao user
+// let userName = prompt('qual é o teu nome?');
+
+// console.log('olá programador '+userName)
+
+//condicionais -> if/else
+// let weekDay = prompt("que dia da semana é hoje?").toLowerCase().trim();
+
+// if (weekDay == "segunda" || weekDay == "segunda-feira") {
+//   alert("força para esta semana!");
+// } else if (weekDay == "quarta" || weekDay == "quarta-feira") {
+//   alert("chegámos ao meio da semana");
+// } else {
+//   alert("já faltou mais para o fim de semana");
+// }
+
+//condicionais -> switch
+// switch (weekDay) {
+//   case "segunda":
+//   case "segunda-feira":
+//     alert("é segunda");
+//     break;
+//   case "terça":
+//   case "terça-feira":
+//     alert("é terça");
+//     break;
+//   case "quarta":
+//   case "quarta-feira":
+//     alert("é quarta");
+//     break;
+//   case "quinta":
+//   case "quinta-feira":
+//     alert("é quinta");
+//     break;
+//   case "sexta":
+//   case "sexta-feira":
+//     alert("é sexta");
+//     break;
+//   case "sábado":
+//   case "domingo":
+//     alert("é fim de semana");
+//     break;
+//   default:
+//     alert("dia inválido");
+// }
+
+//array de strings
+let alunos = ['Maria Cardoso', 'Bruno Azevedo', 'Beatriz Afonso'];
+
+//array de números
+let notas = [20, 15, 9];
+
+//array com vários tipos de dados: string, número e boleano
+let mixedArray=['Cesae', 15, true]; 
+
+//alert(`a aluna ${alunos[2]} tem nota ${notas[2]}`);
+
+alunos[1] = 'Bruno Silva';
+
+
+// //perguntar ao user que index quer alterar
+// let index = prompt('qual é o index a alterar?');
+
+//perguntar ao user o novo nome
+// let newName = prompt('qual é o novo nome?');
+
+//alterar no array
+// alunos[index] = newName;
+
+//mostrar a ver se está ok
+//alert(alunos);
+
+//adicionar no fim do array
+alunos.push('Andreia Ricardo');
+
+
+//Perguntar ao utilziador o nome que quer adicionar
+let pushName = prompt('Qual é o novo aluno?');
+alunos.push(pushName)
+
+
+//remover do início
+alunos.shift()
+alert(alunos);
